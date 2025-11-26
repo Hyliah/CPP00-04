851112fa6666601d66687feff63124a4ccfb2fee
Hygie <lhygie@gmail.com>
2025-11-26 14:29:05 +0100

    next

--- CPP_01/ex01/zombieHorde.cpp
+++ CPP_01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: hlichten <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 16:54:17 by hlichten          #+#    #+#             */
-/*   Updated: 2025/11/20 15:40:41 by hlichten         ###   ########.fr       */
+/*   Updated: 2025/11/25 21:10:12 by hlichten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ Zombie* zombieHorde(int N, const std::string& name)
 	Zombie* horde = static_cast<Zombie*>(raw);
 
 	for (int i = 0; i < N; i++)
-		new (&horde[i]) Zombie(name + '_' + std::to_string(i));
+		new (&horde[i]) Zombie(name);
 	return (&horde[0]);
 }
