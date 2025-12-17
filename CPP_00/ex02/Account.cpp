fcc5d2c161f7644977001b8bab6ccf1bfa507a02
Hygie <lhygie@gmail.com>
2025-12-17 22:06:56 +0100

    CPP00 ex02 done

--- CPP_00/ex02/Account.cpp
+++ CPP_00/ex02/Account.cpp
@@ -6,23 +6,51 @@
 /*   By: hlichten <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 22:20:34 by hlichten          #+#    #+#             */
-/*   Updated: 2025/11/14 15:01:46 by hlichten         ###   ########.fr       */
+/*   Updated: 2025/12/17 21:55:28 by hlichten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
+#include <ctime>
+#include <iomanip>
 
-Account::Account(int initial_deposit){
+/*
+Class Account that contain the logic and static variables
+Account::t -> data of one account
+Class with substructurs if I can say so
+All accounts "Account::t" are manipulated by the Account class
+*/
 
+
+//initilisation of all static variables
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+// constructors and destructors
+Account::Account(int initial_deposit) : _amount(initial_deposit){
+    _accountIndex = _nbAccounts;
+    _nbAccounts++;
+    _totalAmount += initial_deposit;
+    _nbDeposits = 0;
+    _nbWithdrawals = 0;
+    _displayTimestamp();
+    std::cout <<"index:" << _accountIndex << ";amount:" << _amount << ";created" <<std::endl;
 }
-
 Account::~Account(void){
+    _nbAccounts--;
+    _totalAmount -= _amount;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
 }
 
+// getters 
 int	Account::getNbAccounts( void ){
 	return (_nbAccounts);
 }
+
 int	Account::getTotalAmount( void ){
     return (_totalAmount);
 }
@@ -32,18 +60,62 @@ int	Account::getNbDeposits( void ){
 int	Account::getNbWithdrawals( void ){
     return (_totalNbWithdrawals);
 }
+
+//19920104_091532
+//[YYYYMMDD_HHMMSS]
+void	Account::_displayTimestamp( void ){
+    std::time_t now = std::time(NULL); //get now time
+    std::tm* ltm = std::localtime(&now); // convert in a tm structure
+
+    std::cout << "[" 
+    << (1900 + ltm->tm_year)
+    << std::setw(2) << std::setfill('0') << (1 + ltm->tm_mon)
+    << std::setw(2) << std::setfill('0') << ltm->tm_mday
+    << "_"
+    << std::setw(2) << std::setfill('0') << ltm->tm_hour
+    << std::setw(2) << std::setfill('0') << ltm->tm_min
+    << std::setw(2) << std::setfill('0') << ltm->tm_sec
+    << "] " ;
+}
+
 void	Account::displayAccountsInfos( void ){
-	std::cout << "index:" << 0 << ";amount:" << 42 << ";deposits:" << 0 <<";withdrawals:" << 0
+    _displayTimestamp();
+    std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
-void	makeDeposit( int deposit ){
+void	Account::makeDeposit( int deposit ){
+    _nbDeposits++;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";deposit:" << deposit << ";amount:" << (_amount + deposit) << ";nb_deposits:" << _nbDeposits << std::endl;
+    _amount += deposit;
+    _totalAmount += deposit;
+    _totalNbDeposits++;
+}
 
-}
-bool	makeWithdrawal( int withdrawal ){
-
-}
-int		checkAmount( void ) const{
-
-}
-void	displayStatus( void ) const{
+// [19920104_091532] index:4;p_amount:1321;withdrawal:76;amount:1245;nb_withdrawals:1
+// [19920104_091532] index:5;p_amount:23;withdrawal:refused
+bool	Account::makeWithdrawal( int withdrawal ){
+    if (_amount < withdrawal){
+        _displayTimestamp();
+        std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";withdrawal:refused" << std::endl;
+        return false;
+    }
+    _nbWithdrawals++;
     
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";withdrawal:" << withdrawal << ";amount:" << (_amount - withdrawal) << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+    
+    _totalNbWithdrawals++;
+    _amount -= withdrawal;
+    _totalAmount -= withdrawal;
+    return true;
 }
+int		Account::checkAmount( void ) const{
+	return _amount;
+}
+void	Account::displayStatus( void ) const{
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits <<";withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+
+//to make the diff : diff <(cut -c18- 19920104_091532.log) <(cut -c18- log_test.log)
--- CPP_00/ex02/tests.cpp
+++ CPP_00/ex02/tests.cpp
@@ -15,31 +15,39 @@
 
 int		main( void ) {
 
-	typedef std::vector<Account::t>							  accounts_t;
-	typedef std::vector<int>								  ints_t;
-	typedef std::pair<accounts_t::iterator, ints_t::iterator> acc_int_t;
+	// typedef for "more readable code" 
+	typedef std::vector<Account::t>							  accounts_t; //account list
+	typedef std::vector<int>								  ints_t; // int list
+	typedef std::pair<accounts_t::iterator, ints_t::iterator> acc_int_t; //to go through an account and an amount in the same time
 
+	//create account with initialization of amount base
+	//every number create an Account::t, all stocked in accounts tab
 	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
 	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
 	accounts_t				accounts( amounts, amounts + amounts_size );
-	accounts_t::iterator	acc_begin	= accounts.begin();
-	accounts_t::iterator	acc_end		= accounts.end();
+	accounts_t::iterator	acc_begin	= accounts.begin(); //set first account
+	accounts_t::iterator	acc_end		= accounts.end(); //set last account
 
+	//amount to deposit in the same order as acount numbers
 	int	const			d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 };
 	size_t const		d_size( sizeof(d) / sizeof(int) );
 	ints_t				deposits( d, d + d_size );
 	ints_t::iterator	dep_begin	= deposits.begin();
 	ints_t::iterator	dep_end		= deposits.end();
 
+	//same but to withdrawals
 	int	const			w[]			= { 321, 34, 657, 4, 76, 275, 657, 7654 };
 	size_t const		w_size( sizeof(w) / sizeof(int) );
 	ints_t				withdrawals( w, w + w_size );
 	ints_t::iterator	wit_begin	= withdrawals.begin();
 	ints_t::iterator	wit_end		= withdrawals.end();
 
+	//display the acconts global informations
 	Account::displayAccountsInfos();
+	//print all detailed information from first to last account
 	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
+	//every account get theirs deposits
 	for ( acc_int_t it( acc_begin, dep_begin );
 		  it.first != acc_end && it.second != dep_end;
 		  ++(it.first), ++(it.second) ) {
@@ -47,9 +55,11 @@ int		main( void ) {
 		(*(it.first)).makeDeposit( *(it.second) );
 	}
 
+	//again to see the changes
 	Account::displayAccountsInfos();
 	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
+	// all the withdrawals of all the accounts 
 	for ( acc_int_t it( acc_begin, wit_begin );
 		  it.first != acc_end && it.second != wit_end;
 		  ++(it.first), ++(it.second) ) {
@@ -57,6 +67,7 @@ int		main( void ) {
 		(*(it.first)).makeWithdrawal( *(it.second) );
 	}
 
+	// and again
 	Account::displayAccountsInfos();
 	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
